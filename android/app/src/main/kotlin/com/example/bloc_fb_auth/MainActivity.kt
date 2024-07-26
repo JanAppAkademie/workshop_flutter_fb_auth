5259533512268e6115ae3fedefcd763f343329d0
JanAppAkademie <jan.biernacki@app-akademie.com>
2024-07-26 11:43:20 +0200

    Updated Application and added DefaultOptions

--- android/app/src/main/kotlin/com/example/bloc_fb_auth/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/bloc_fb_auth/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.bloc_fb_auth
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
---
+++ lib/auth/ui/home_screen.dart
@@ -0,0 +1,78 @@
+import 'package:bloc_fb_auth/auth/bloc/auth_cubit.dart';
+import 'package:bloc_fb_auth/auth/bloc/auth_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Google Registration"),
+              ElevatedButton(
+                onPressed: () {
+                  context.read<AuthCubit>().signInWithGoogle(context);
+                },
+                child: const Text("Sign In with Google"),
+              ),
+              const SizedBox(height: 20),
+              BlocConsumer<AuthCubit, AuthState>(
+                listener: (context, state) {
+                  if (state is AuthError) {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text(state.message),
+                      duration: const Duration(seconds: 1),
+                    ));
+                  }
+                },
+                builder: (context, state) {
+                  return Form(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            controller: _emailController,
+                            decoration:
+                                const InputDecoration(labelText: 'Email'),
+                          ),
+                          TextFormField(
+                            controller: _passwordController,
+                            decoration:
+                                const InputDecoration(labelText: 'Password'),
+                            obscureText: true,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          ElevatedButton(
+                            onPressed: () => context
+                                .read<AuthCubit>()
+                                .loginWithEmailPassword(
+                                  _emailController.text,
+                                  _passwordController.text,
+                                  context,
+                                ),
+                            child: const Text('Log In'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
